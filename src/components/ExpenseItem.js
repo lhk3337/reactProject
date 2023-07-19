ffa2ee3ce4ae37db026a369f272094d193e3660a
lhk3337 <lhk3337@naver.com>
2023-07-19 21:21:53 +0900

    [FEAT] useState설정하기

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
-const ExpenseItem = ({ title, amount, date }) => {
+const ExpenseItem = ({ title: propTitle, amount, date }) => {
+  const [title, setTitle] = useState(propTitle);
+  // console.log("ExpenseItem evaluated by React");
+
+  const onHandleClick = () => {
+    setTitle("Updated!");
+    // console.log(title);
+  };
+
   return (
     <div className="expense-item">
       <ExpenseDate date={date} />
@@ -9,6 +17,7 @@ const ExpenseItem = ({ title, amount, date }) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={onHandleClick}>Change Title</button>
     </div>
   );
 };
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 const Expenses = ({ data }) => {
-  console.log(data);
   return (
     <div className="expenses">
       {data.map((item) => (
