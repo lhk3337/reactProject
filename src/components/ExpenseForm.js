1e3bf3fd19fae6c0487eaa5d670995f5857d8e76
lhk3337 <lhk3337@naver.com>
2023-07-19 22:55:36 +0900

    [FEAT] form event 설정하기

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./ExpenseForm.css";
+const ExpenseForm = ({ onSaveExpenseDataHandler }) => {
+  const [values, setValues] = useState({
+    title: "",
+    amount: 0.01,
+    date: new Date().toISOString().split("T")[0],
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSaveExpenseDataHandler(values);
+  };
+
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" name="title" onChange={handleChange} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" value={values.amount} name="amount" min="0.01" step="0.01" onChange={handleChange} />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            value={values.date}
+            type="date"
+            min="2022-01-01"
+            max="'2023-12-31"
+            name="date"
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
