165a35412a758d1c1419b6c5b87a7f1fe4e603e9
lhk3337 <lhk3337@naver.com>
2023-07-20 21:37:25 +0900

    [FEAT] children props 설정해보기

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 const Expenses = ({ data }) => {
   return (
-    <div className="expenses">
-      {data.map((item) => (
-        <ExpenseItem key={item.id} {...item} />
-      ))}
-    </div>
+    <>
+      <Card className="expenses">
+        {data.map((item) => (
+          <ExpenseItem key={item.id} {...item} />
+        ))}
+      </Card>
+    </>
   );
 };
 
