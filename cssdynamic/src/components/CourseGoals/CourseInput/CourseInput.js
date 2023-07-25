9a36a1ac68e2e60afbdb496b7d29a2b8e255ffe8
lhk3337 <lhk3337@naver.com>
2023-07-25 20:16:00 +0900

    [FEAT] class name으로 조건에 따른 동적 css 설정하기

--- cssdynamic/src/components/CourseGoals/CourseInput/CourseInput.js
+++ cssdynamic/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -26,16 +26,9 @@ const CourseInput = ({ onAddGoal }) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${isemptyValue ? "invalid" : ""}`}>
         <label style={{ color: isemptyValue ? "red" : "black" }}>Course Goal</label>
-        <input
-          style={{
-            border: `2px solid ${isemptyValue ? "#900c0c" : "black"}`,
-            backgroundColor: isemptyValue ? "#ff4040" : "#fad0ec",
-          }}
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
