45794d8d81bf0b89a6c7d3f1e48af00a68d73ddf
lhk3337 <lhk3337@naver.com>
2023-07-25 20:13:48 +0900

    [FEAT] inline style로  조건에 따른 동적 css 설정하기

--- cssdynamic/src/components/CourseGoals/CourseInput/CourseInput.js
+++ cssdynamic/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,21 +5,37 @@ import "./CourseInput.css";
 
 const CourseInput = ({ onAddGoal }) => {
   const [enteredValue, setEnteredValue] = useState("");
-
+  const [isemptyValue, setIsemptyValue] = useState(false);
   const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
+    if (enteredValue.trim().length === 0) {
+      setIsemptyValue(false);
+    }
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
-    onAddGoal(enteredValue);
+
+    if (enteredValue.trim().length === 0) {
+      setIsemptyValue(true);
+    } else {
+      onAddGoal(enteredValue);
+      setIsemptyValue(false);
+    }
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: isemptyValue ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{
+            border: `2px solid ${isemptyValue ? "#900c0c" : "black"}`,
+            backgroundColor: isemptyValue ? "#ff4040" : "#fad0ec",
+          }}
+          type="text"
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
