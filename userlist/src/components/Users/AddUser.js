17db9f9a9ba7edf7818122545b47916446f191ba
lhk3337 <lhk3337@naver.com>
2023-07-30 20:14:41 +0900

    [FEAT] 사용자 입력 handler 커링 함수로 설정하기

--- userlist/src/components/Users/AddUser.js
+++ userlist/src/components/Users/AddUser.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 const AddUser = () => {
+  const [user, setUser] = useState("");
+  const [age, setAge] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(user, age);
+  };
+
+  const handleOnChange = (setState) => (event) => {
+    setState(event.target.value);
+  }; // currying function
+
   return (
     <Card className={classes.input}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" value={user} onChange={handleOnChange(setUser)} />
         <label htmlFor="age">Age(Year)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" value={age} onChange={handleOnChange(setAge)} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
