2ab235ca6f717c4fbf2cb6109c3ccda2ff0b7738
lhk3337 <lhk3337@naver.com>
2023-08-01 18:54:21 +0900

    [FEAT] result investment 나타내기

---
+++ practice_prj/calculator/src/components/InvestmentList.js
@@ -0,0 +1,39 @@
+import React from "react";
+const formatter = new Intl.NumberFormat("ko-KR", {
+  style: "currency",
+  currency: "KRW",
+});
+const InvestmentList = ({ datas, initialInvest }) => {
+  return (
+    <div>
+      <table className="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Total Savings</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          {datas.map((data) => (
+            <tr key={data.year}>
+              <td>{data.year}</td>
+              <td>{formatter.format(Math.round(data.savingsEndOfYear))}</td>
+              <td>{formatter.format(Math.round(data.yearlyInterest))}</td>
+              <td>
+                {formatter.format(
+                  Math.round(data.savingsEndOfYear - initialInvest - data.yearlyContribution * data.year)
+                )}
+              </td>
+              <td>{formatter.format(Math.round(initialInvest + data.yearlyContribution * data.year))}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default InvestmentList;
