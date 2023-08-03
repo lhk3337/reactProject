1809b49efbf6da8f3acbd88916e2d326da7fc2dd
lhk3337 <lhk3337@naver.com>
2023-08-03 21:51:41 +0900

    [FEAT] 의존성 없는 useEffect 선언하기

--- react_advance/src/App.js
+++ react_advance/src/App.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const loggedUser = localStorage.getItem("isLoggedIn");
+
+    if (loggedUser === "LOGGED_IN") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "LOGGED_IN");
+    // localStorage.setItem("isLoggedIn", "LOGGED_OUT");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
