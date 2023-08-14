5072803944b04cd48414acf8a48cff5c4ba08c07
lhk3337 <lhk3337@naver.com>
2023-08-14 22:34:36 +0900

    [FEAT] Foods 컴포넌트 추가하기

--- practice_prj/food_order_app/src/App.js
+++ practice_prj/food_order_app/src/App.js
@@ -1,8 +1,12 @@
 import Header from "./components/Layout/Header";
+import Foods from "./components/Food/Foods";
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Foods />
+      </main>
     </>
   );
 }
---
+++ practice_prj/food_order_app/src/components/Food/Foods.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FoodsSummary from "./FoodsSummary";
+import AvailableFoods from "./AvailableFoods";
+
+const Foods = () => {
+  return (
+    <>
+      <FoodsSummary />
+      <AvailableFoods />
+    </>
+  );
+};
+
+export default Foods;
