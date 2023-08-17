75079377a1e5025866a77fc99ba5e20fd96bec84
lhk3337 <lhk3337@naver.com>
2023-08-17 20:44:47 +0900

    [FEAT] Add 버튼 클릭 시 Cart의 카운터 증가 시키기

--- practice_prj/food_order_app/src/components/Food/FoodItem/FoodItem.js
+++ practice_prj/food_order_app/src/components/Food/FoodItem/FoodItem.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./FoodItem.module.css";
+import CartContext from "../../../store/cart-context";
 import FoodItemForm from "./FoodItemForm";
+
 const FoodItem = ({ name, id, description, price: priceValue }) => {
+  const cartCtx = useContext(CartContext);
   const price = `${priceValue.toFixed(2)}`;
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id,
+      name,
+      amount,
+      price: priceValue,
+    });
+  };
   return (
     <li className={classes.food}>
       <div>
@@ -11,7 +22,7 @@ const FoodItem = ({ name, id, description, price: priceValue }) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <FoodItemForm id={id} />
+        <FoodItemForm id={id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- practice_prj/food_order_app/src/components/Food/FoodItem/FoodItemForm.js
+++ practice_prj/food_order_app/src/components/Food/FoodItem/FoodItemForm.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./FoodItemForm.module.css";
 import Input from "../../UI/Input";
-const FoodItemForm = ({ id }) => {
+const FoodItemForm = ({ id, onAddToCart }) => {
+  const amountInputRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+    console.log(enteredAmountNumber);
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+    onAddToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
         label="Amount"
+        ref={amountInputRef}
         input={{ id: `amount_${id}`, type: "number", min: "1", max: "5", step: "1", defaultValue: "1" }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
