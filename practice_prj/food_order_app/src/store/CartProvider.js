81712f1bf61575717ab6ff4ea669e071c8d383ad
lhk3337 <lhk3337@naver.com>
2023-08-19 18:44:48 +0900

    [FEAT] 항목 중복 추가 대신 숫자 카운트만 증가 되게 설정하기

--- practice_prj/food_order_app/src/store/CartProvider.js
+++ practice_prj/food_order_app/src/store/CartProvider.js
@@ -6,12 +6,28 @@ const defaultCartState = {
 };
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+    if (existingCartItem) {
+      let updatedItem;
+      updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+
     return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
   return defaultCartState;
 };
+
 const CartProvider = ({ children }) => {
   const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
   const addItemToCartHandler = (item) => {
