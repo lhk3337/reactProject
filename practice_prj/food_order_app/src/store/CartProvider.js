dc82ca4cdeb64964b54ed42ec004a7123f2289b6
lhk3337 <lhk3337@naver.com>
2023-08-21 17:56:28 +0900

    [DOCS] cart provider remove 타입comments 추가하기

--- practice_prj/food_order_app/src/store/CartProvider.js
+++ practice_prj/food_order_app/src/store/CartProvider.js
@@ -26,16 +26,16 @@ const cartReducer = (state, action) => {
     return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
   if (action.type === "REMOVE") {
-    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
-    const existingItem = state.items[existingCartItemIndex];
-    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id); // 선택될 요소 배열 인덱스를 가지고 옴
+    const existingItem = state.items[existingCartItemIndex]; // 선택된 요소를 existingItem에 저장
+    const updatedTotalAmount = state.totalAmount - existingItem.price; //현재 totalAmount에서 선택된 요소의 가격을 뺌
     let updatedItems;
     if (existingItem.amount === 1) {
-      updatedItems = state.items.filter((item) => item.id !== action.id);
+      updatedItems = state.items.filter((item) => item.id !== action.id); // id가 같은 해당 요소 삭제
     } else {
-      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
-      updatedItems = [...state.items];
-      updatedItems[existingCartItemIndex] = updatedItem;
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }; // amount요소를 하나씩 뺌
+      updatedItems = [...state.items]; // 변경되지 않는 부분은 나둠
+      updatedItems[existingCartItemIndex] = updatedItem; // 변경된 부분만 업데이트
     }
     return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
