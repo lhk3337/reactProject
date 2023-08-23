344f752036be0a9baa5bc434fea778dc97c1bde4
lhk3337 <lhk3337@naver.com>
2023-08-23 20:19:49 +0900

    [FEAT] 자식 컴포넌트 재평가 확인하기

--- react-behind/src/App.js
+++ react-behind/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
@@ -12,10 +13,14 @@ function App() {
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      {/* {showParagraph && <p>This is new!</p>} */}
+      <DemoOutput show={showParagraph} />
       <Button onClick={toggleParagraphHandler}>Toogle Pragraph!</Button>
     </div>
   );
 }
 
 export default App;
+
+// 부모 컴포넌트의 상태가 변경되면 재실행 되는데 이때 자식 컴포넌트도 재실행 된다.
+// 이유는 자식 컴포넌트는 return문 안에 JSX형태로 있고 이것을 다시 함수 호출처럼 실행한다.
