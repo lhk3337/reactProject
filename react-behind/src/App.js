a05d9df6d0889aa06c96f37b0f6d05558fb941c9
lhk3337 <lhk3337@naver.com>
2023-08-24 10:13:32 +0900

    [FEAT] memo 이해하기

--- react-behind/src/App.js
+++ react-behind/src/App.js
@@ -14,13 +14,15 @@ function App() {
     <div className="app">
       <h1>Hi there!</h1>
       {/* {showParagraph && <p>This is new!</p>} */}
-      <DemoOutput show={showParagraph} />
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toogle Pragraph!</Button>
     </div>
   );
 }
 
 export default App;
-
-// 부모 컴포넌트의 상태가 변경되면 재실행 되는데 이때 자식 컴포넌트도 재실행 된다.
-// 이유는 자식 컴포넌트는 return문 안에 JSX형태로 있고 이것을 다시 함수 호출처럼 실행한다.
+/**
+ * * 자식 컴포넌트 재평가
+ * * 부모 컴포넌트의 상태가 변경되면 재실행 되는데 이때 자식 컴포넌트도 재실행 된다.
+ *  * 이유는 자식 컴포넌트는 return문 안에 JSX형태로 있고 이것을 다시 함수 호출처럼 실행한다.
+ */
--- react-behind/src/components/Demo/DemoOutput.js
+++ react-behind/src/components/Demo/DemoOutput.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 import MyParagraph from "./MyParagraph";
 
 const DemoOutput = ({ show }) => {
@@ -6,4 +6,10 @@ const DemoOutput = ({ show }) => {
   return <MyParagraph>{show ? "This is new!" : ""}</MyParagraph>;
 };
 
-export default DemoOutput;
+export default memo(DemoOutput);
+
+/**
+ * memo
+ * * memo는 props 값에서 원시값(number, string, boolean)등에 대해 이전 값과 상태가 변경된 값이 같으면 재평가를 하지 않는다.
+ * * 하지만 memo는 함수 객체 배열 같은 참조값들은 재평가를 하지 못한다.
+ */
