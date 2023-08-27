cca15c7f3ac276565e3d59ad109838e6d9505035
lhk3337 <lhk3337@naver.com>
2023-08-27 21:45:56 +0900

    [FEAT] useMemo 최적화 하기 위한 초기화

--- react-behind/src/App.js
+++ react-behind/src/App.js
@@ -1,27 +1,19 @@
 import React, { useState, useCallback } from "react";
 import Button from "./components/UI/Button/Button";
+import DemoList from "./components/Demo/DemoList";
 import "./App.css";
-import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
-  const [showParagraph, setShowParagraph] = useState(false);
-  const [allowToggle, setAllowToggle] = useState(false);
-  console.log("APP RUNNING");
-  const toggleParagraphHandler = useCallback(() => {
-    if (allowToggle) {
-      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-    }
-  }, [allowToggle]);
-  const allowToggleHandler = () => {
-    setAllowToggle(true);
-  };
+  const [listTitle, setListTitle] = useState("My List");
+
+  const changeTitleHandler = useCallback(() => {
+    setListTitle("New Title");
+  }, []);
+
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      {/* {showParagraph && <p>This is new!</p>} */}
-      <DemoOutput show={showParagraph} />
-      <Button onClick={allowToggleHandler}>Allow Toogling!</Button>
-      <Button onClick={toggleParagraphHandler}>Toogle Pragraph!</Button>
+      <DemoList title={listTitle} items={[5, 3, 1, 10, 9]} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
