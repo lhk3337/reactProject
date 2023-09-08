8560103466fa4b6fd5822357ec99e885b48e3589
lhk3337 <lhk3337@naver.com>
2023-09-08 23:17:44 +0900

    [FEAT] get 방식 useHttp 적용하기

--- custom_hook/realistic/src/App.js
+++ custom_hook/realistic/src/App.js
@@ -2,35 +2,19 @@ import React, { useEffect, useState } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
-
+import useHttp from "./hooks/use-http";
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
+  const transformTasks = (taskObj) => {
+    const loadedTasks = [];
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(process.env.REACT_APP_DB_HOST);
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp({}, transformTasks);
 
   useEffect(() => {
     fetchTasks();
