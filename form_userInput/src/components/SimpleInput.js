c7ae1f7b1b9c9f73faefaccb169c744b811300f7
lhk3337 <lhk3337@naver.com>
2023-09-10 20:33:49 +0900

    [FEAT] onSubmit user input value 설정하기

--- form_userInput/src/components/SimpleInput.js
+++ form_userInput/src/components/SimpleInput.js
@@ -1,9 +1,27 @@
+import { useRef, useState } from "react";
 const SimpleInput = (props) => {
+  const [enterName, setEnterName] = useState("");
+  const nameInputRef = useRef();
+  const nameInputChangeHandler = (event) => {
+    setEnterName(event.target.value);
+  };
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log("stateValue :", enterName);
+
+    const enteredValued = nameInputRef.current.value;
+    console.log("refValue :", enteredValued);
+    setEnterName("");
+    /**
+     * * ref를 이용한 input value 초기화
+     * TODO nameInputRef.current.value = "";
+     */
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" value={enterName} onChange={nameInputChangeHandler} ref={nameInputRef} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
@@ -13,3 +31,6 @@ const SimpleInput = (props) => {
 };
 
 export default SimpleInput;
+
+// ref는 여러 입력값을 추합하여 onSubmit할때 이 값을 한번 할때 유용하다
+// 즉각적인 유효성 검증을 위해서는 useState를 사용하는 것이 유용하다.
