5989912e4ce8578013de1854baf46f7fe202ddb1
lhk3337 <lhk3337@naver.com>
2023-09-19 20:48:03 +0900

    [FEAT] fetch get방식으로 데이터 가져와 렌더링하기

--- practice_prj/food_order_app/src/components/Food/AvailableFoods.js
+++ practice_prj/food_order_app/src/components/Food/AvailableFoods.js
@@ -1,35 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./AvailableFoods.module.css";
 import Card from "../UI/Card";
 import FoodItem from "./FoodItem/FoodItem";
-const DUMMY_FOODS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+
 const AvailableFoods = () => {
-  const foodsList = DUMMY_FOODS.map((food) => <FoodItem key={food.id} {...food} />);
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(process.env.REACT_APP_DB_HOST);
+      const responseData = await response.json();
+      const loadedMeals = [];
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+    };
+    fetchMeals();
+  }, []);
+
+  const foodsList = meals.map((food) => <FoodItem key={food.id} {...food} />);
   return (
     <section className={classes.foods}>
       <Card>
