4c148508183a271332695f11a88e154ed1861d6f
lhk3337 <lhk3337@naver.com>
2023-09-20 15:47:16 +0900

    [FEAT] api 데이터 예외처리하기

--- practice_prj/food_order_app/src/components/Food/AvailableFoods.js
+++ practice_prj/food_order_app/src/components/Food/AvailableFoods.js
@@ -6,27 +6,76 @@ import FoodItem from "./FoodItem/FoodItem";
 const AvailableFoods = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+  /**
+   * TODO promise
+   */
+  const fetchMeals = async () => {
+    const response = await fetch(process.env.REACT_APP_DB_HOST);
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+    const responseData = await response.json();
+    const loadedMeals = [];
+    for (const key in responseData) {
+      loadedMeals.push({
+        id: key,
+        name: responseData[key].name,
+        description: responseData[key].description,
+        price: responseData[key].price,
+      });
+    }
+    setMeals(loadedMeals);
+    setIsLoading(false);
+  };
+
   useEffect(() => {
-    const fetchMeals = async () => {
-      const response = await fetch(process.env.REACT_APP_DB_HOST);
-      const responseData = await response.json();
-      const loadedMeals = [];
-      for (const key in responseData) {
-        loadedMeals.push({
-          id: key,
-          name: responseData[key].name,
-          description: responseData[key].description,
-          price: responseData[key].price,
-        });
-      }
-      setMeals(loadedMeals);
+    fetchMeals().catch((error) => {
       setIsLoading(false);
-    };
-    fetchMeals();
+      setHttpError(error.message);
+    });
   }, []);
 
-  const foodsList = meals.map((food) => <FoodItem key={food.id} {...food} />);
+  /**
+   * TODO try catch
+   */
 
+  // const fetchMeals = async () => {
+  //   try {
+  //     const response = await fetch(process.env.REACT_APP_DB_HOST);
+  //     if (!response.ok) {
+  //       throw new Error("Something went wrong!");
+  //     }
+  //     const responseData = await response.json();
+  //     const loadedMeals = [];
+  //     for (const key in responseData) {
+  //       loadedMeals.push({
+  //         id: key,
+  //         name: responseData[key].name,
+  //         description: responseData[key].description,
+  //         price: responseData[key].price,
+  //       });
+  //     }
+  //     setMeals(loadedMeals);
+  //     setIsLoading(false);
+  //   } catch (error) {
+  //     setIsLoading(false);
+  //     setHttpError(error.message);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchMeals();
+  // }, []);
+
+  const foodsList = meals.map((food) => <FoodItem key={food.id} {...food} />);
+  if (httpError) {
+    return (
+      <section className={classes.mealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
   return (
     <section className={classes.foods}>
       <Card>{isLoading ? <div className={classes.dualring} /> : <ul>{foodsList}</ul>}</Card>
