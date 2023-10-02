f04c1d1f60e94ea6e88af54777571e07e8d66050
lhk3337 <lhk3337@naver.com>
2023-10-02 20:57:12 +0900

    [FEAT] 여러 slice 설정 및 useSelector 및 dispatch하기

--- redux/react/src/store/index.js
+++ redux/react/src/store/index.js
@@ -21,8 +21,24 @@ const counterSlice = createSlice({
   },
 });
 
-const store = configureStore({ reducer: counterSlice.reducer });
+const initialAuthState = { isAuthenticated: false };
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+const store = configureStore({ reducer: { counter: counterSlice.reducer, auth: authSlice.reducer } });
 
 export const counterActions = counterSlice.actions;
+
+export const authActions = authSlice.actions;
 
 export default store;
