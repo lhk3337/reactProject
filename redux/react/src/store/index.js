686bdbb4cbad2ea63cbe83fc9f7cae0a7c87fb39
lhk3337 <lhk3337@naver.com>
2023-10-03 22:05:25 +0900

    [FIX] redux slice devided

--- redux/react/src/store/index.js
+++ redux/react/src/store/index.js
@@ -1,44 +1,7 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter";
+import authReducer from "./auth";
 
-const initialState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = { isAuthenticated: false };
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
-
-const store = configureStore({ reducer: { counter: counterSlice.reducer, auth: authSlice.reducer } });
-
-export const counterActions = counterSlice.actions;
-
-export const authActions = authSlice.actions;
+const store = configureStore({ reducer: { counter: counterReducer, auth: authReducer } });
 
 export default store;
