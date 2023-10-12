48dc6679d503a063b62797c8c424d87f770e31d6
lhk3337 <lhk3337@naver.com>
2023-10-12 22:59:48 +0900

    [FEAT] second route 설정하기

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,7 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./page/Home";
+import ProductsPage from "./page/Products";
 function App() {
-  const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+  const router = createBrowserRouter([
+    { path: "/", element: <HomePage /> },
+    { path: "/products", element: <ProductsPage /> },
+  ]);
 
   return <RouterProvider router={router} />;
 }
