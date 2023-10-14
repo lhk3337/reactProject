4bfc9380cb024a51d42ed638166b26df2dbcbea6
lhk3337 <lhk3337@naver.com>
2023-10-14 20:48:25 +0900

    [FEAT] Link를 이용한 페이지 이동

--- react-router/src/page/Home.js
+++ react-router/src/page/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const HomePage = () => {
-  return <h1>My Home Page</h1>;
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        a tag Goto <a href="/products">the last of products</a>
+        {/* 새로운 HTTP 요청을 서버에 전송후 페이지 다시 시작 */}
+      </p>
+      <p>
+        Link Goto <Link to="/products">the last of products</Link>
+        {/* 새로운 HTTP 요청을 방지하고자 Link를 이용해 페이지 이동한다. */}
+      </p>
+    </>
+  );
 };
 
 export default HomePage;
