eaffda8fc616365595ecdbd61514e1760d1b0a2a
lhk3337 <lhk3337@naver.com>
2023-10-18 20:59:48 +0900

    [FEAT] navigation handler

--- react-router/src/page/Home.js
+++ react-router/src/page/Home.js
@@ -1,6 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate("/products");
+  };
   return (
     <>
       <h1>My Home Page</h1>
@@ -11,6 +16,10 @@ const HomePage = () => {
       <p>
         Link Goto <Link to="/products">the last of products</Link>
         {/* 새로운 HTTP 요청을 방지하고자 Link를 이용해 페이지 이동한다. */}
+      </p>
+
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
