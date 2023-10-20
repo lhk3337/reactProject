23e0a8d681c8447b9a8e44427d9fcca24b8bc210
lhk3337 <lhk3337@naver.com>
2023-10-21 00:45:35 +0900

    [FEAT] index route

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <HomePage /> },
+      { index: true, element: <HomePage /> }, // index: true, 부모 route(여기서는 "/")가 활성화 될 경우 로딩이 되어야 하는 기본 라우트를 정의한다.
       { path: "/products", element: <ProductsPage /> },
       { path: "/products/:productId", element: <ProductDetailPage /> },
     ],
