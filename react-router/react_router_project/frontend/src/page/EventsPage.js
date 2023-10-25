d95749160919243727d9459022f6af34250997e2
lhk3337 <lhk3337@naver.com>
2023-10-25 22:59:17 +0900

    [FEAT] react route loader를 이용해서 api데이터 가져오기

--- react-router/react_router_project/frontend/src/page/EventsPage.js
+++ react-router/react_router_project/frontend/src/page/EventsPage.js
@@ -1,22 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
-const DUMMY_EVENTS = [
-  { id: "e1", title: "Some event" },
-  { id: "e2", title: "Another event" },
-];
-const EventsPage = () => {
+import { useLoaderData } from "react-router-dom";
+
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+  const events = useLoaderData();
   return (
     <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={`/events/${event.id}`}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   );
-};
+}
 
 export default EventsPage;
