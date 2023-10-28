a037a63b7b8cf1071156008e15f12c90fa0d2827
lhk3337 <lhk3337@naver.com>
2023-10-28 19:32:11 +0900

    [FEAT] HTTP status에 따라 에러 메시지 보내기

--- react-router/react_router_project/frontend/src/page/EventsPage.js
+++ react-router/react_router_project/frontend/src/page/EventsPage.js
@@ -18,7 +18,7 @@ export async function loader() {
 
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events." };
-    throw { message: "Could not fetch events." };
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), { status: 500 });
   } else {
     const resData = await response.json();
     return resData.events;
