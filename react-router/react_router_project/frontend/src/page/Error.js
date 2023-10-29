706e096a575a85c46ffb39ae2509708254d1ec2a
lhk3337 <lhk3337@naver.com>
2023-10-29 15:09:08 +0900

    [FEAT] react-route json()로 HTTP status 처리하기

--- react-router/react_router_project/frontend/src/page/Error.js
+++ react-router/react_router_project/frontend/src/page/Error.js
@@ -7,7 +7,8 @@ const ErrorPage = () => {
   let title = "An error occured!";
   let message = "Something went wrong!";
   if (error.status === 500) {
-    message = JSON.parse(error.data).message;
+    // message = JSON.parse(error.data).message;
+    message = error.data.message;
   }
   if (error.status === 404) {
     title = "Not found!";
--- react-router/react_router_project/frontend/src/page/EventsPage.js
+++ react-router/react_router_project/frontend/src/page/EventsPage.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
@@ -14,11 +14,12 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/eventss");
+  const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events." };
-    throw new Response(JSON.stringify({ message: "Could not fetch events." }), { status: 500 });
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), { status: 500 });
+    throw json({ message: "Could not fetch events." }, { status: 500 });
   } else {
     const resData = await response.json();
     return resData.events;
