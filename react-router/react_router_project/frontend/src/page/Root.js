171097fba1463f3341afc3599d9fd215c578fff3
lhk3337 <lhk3337@naver.com>
2023-10-30 21:25:12 +0900

    [FIX] loading sttate cancel

--- react-router/react_router_project/frontend/src/page/Root.js
+++ react-router/react_router_project/frontend/src/page/Root.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Outlet, useNavigation } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 const RootLayout = () => {
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
   return (
     <>
       <MainNavigation />
       <main>
-        {navigation.state === "loading" && <p>Loading...</p>}
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
         <Outlet />
       </main>
     </>
