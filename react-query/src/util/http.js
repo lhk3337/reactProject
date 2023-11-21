a2b89f3a7b23ce9a376e413901077dd160650d19
lhk3337 <lhk3337@naver.com>
2023-11-21 22:06:55 +0900

    [FEAT] useQuery로 image 가져오기

--- react-query/src/util/http.js
+++ react-query/src/util/http.js
@@ -38,3 +38,19 @@ export async function createNewEvent(eventData) {
 
   return event;
 }
+export async function fetchSelectableImages({ signal }) {
+  const response = await fetch(`http://localhost:3000/events/images`, { signal });
+  console.log(signal);
+  console.log(response);
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the images");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { images } = await response.json();
+
+  return images;
+}
