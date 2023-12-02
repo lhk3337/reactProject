1079d0de8f808c80b11d98316d68b430ad56a486
lhk3337 <lhk3337@naver.com>
2023-12-02 11:12:38 +0900

    [FEAT] page link with nextjs

--- nextjs/pages/news/index.js
+++ nextjs/pages/news/index.js
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-good-framework">NextJS Is A Good Framework</Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </>
+  );
 }
