a2b3f920842ce74c4509965fd360e57c6d76e91f
lhk3337 <lhk3337@naver.com>
2023-12-04 18:29:45 +0900

    [FEAT] add meetup detail page

---
+++ nextjs/nextjsproject/pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+function meetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export default meetupDetails;
