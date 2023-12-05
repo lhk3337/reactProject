e553ea67daffb2c8165e0e76bb49d6dba670af3d
lhk3337 <lhk3337@naver.com>
2023-12-05 19:34:31 +0900

    [FEAT] pre rendering type

--- nextjs/nextjsproject/pages/index.js
+++ nextjs/nextjsproject/pages/index.js
@@ -1,23 +1,30 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "First MeetUp",
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some Address 1, 1234",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "Second MeetUp",
+    image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some Address 1, 1234",
+    description: "This is a second meetup",
+  },
+];
 function HomePage() {
-  const DUMMY_MEETUPS = [
-    {
-      id: "m1",
-      title: "First MeetUp",
-      image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-      address: "Some Address 1, 1234",
-      description: "This is a first meetup",
-    },
-    {
-      id: "m2",
-      title: "Second MeetUp",
-      image: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg",
-      address: "Some Address 1, 1234",
-      description: "This is a second meetup",
-    },
-  ];
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setLoadedMeetups(DUMMY_MEETUPS);
+  }, []);
+
+  return <MeetupList meetups={loadedMeetups} />;
 }
 
 export default HomePage;
