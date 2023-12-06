6dd0d82ee1b5b060c8a1f47faada8bbe49bb8af5
lhk3337 <lhk3337@naver.com>
2023-12-06 23:55:16 +0900

    [FEAT] mongodb set

--- nextjs/nextjsproject/pages/api/new-meetup.js
+++ nextjs/nextjsproject/pages/api/new-meetup.js
@@ -1,10 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect("mongodb://localhost:27017/meetupdb");
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 export default handler;
