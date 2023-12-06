def690fe79defe7d48b2cb32c30c4e075d67275a
lhk3337 <lhk3337@naver.com>
2023-12-06 23:47:23 +0900

    [FEAT] nextjs api

---
+++ nextjs/nextjsproject/pages/api/new-meetup.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+    const { title, image, address, description } = data;
+  }
+}
+export default handler;
