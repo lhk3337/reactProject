d0436c5d3177e4d7491a42ac9ef5870d7e0ec49b
lhk3337 <lhk3337@naver.com>
2023-12-12 19:27:59 +0900

    [FEAT] framer motion rotate

--- react-animation/framemotion-prj/src/components/ChallengeItem.jsx
+++ react-animation/framemotion-prj/src/components/ChallengeItem.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-
+import { motion } from "framer-motion";
 import { ChallengesContext } from "../store/challenges-context.jsx";
 
 export default function ChallengeItem({ challenge, onViewDetails, isExpanded }) {
@@ -38,7 +38,10 @@ export default function ChallengeItem({ challenge, onViewDetails, isExpanded }) 
         <div className="challenge-item-details">
           <p>
             <button onClick={onViewDetails}>
-              View Details <span className="challenge-item-details-icon">&#9650;</span>
+              View Details
+              <motion.span animate={{ rotate: isExpanded ? 180 : 0 }} className="challenge-item-details-icon">
+                &#9650;
+              </motion.span>
             </button>
           </p>
 
