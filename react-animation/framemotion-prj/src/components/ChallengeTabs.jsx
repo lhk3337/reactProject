f12609e98026ee62201ab994bc68ca1fe65a3bf3
lhk3337 <lhk3337@naver.com>
2023-12-19 18:48:46 +0900

    [FEAT] item list and tab bar animated with framer-motion

--- react-animation/framemotion-prj/src/components/ChallengeTabs.jsx
+++ react-animation/framemotion-prj/src/components/ChallengeTabs.jsx
@@ -1,46 +1,38 @@
-import Badge from './Badge.jsx';
-
+import Badge from "./Badge.jsx";
+import { motion } from "framer-motion";
 function Tab({ isSelected, onSelect, badgeCaption, children }) {
   return (
     <li>
-      <button
-        className={isSelected ? 'selected' : undefined}
-        onClick={onSelect}
-      >
+      <button className={isSelected ? "selected" : undefined} onClick={onSelect}>
         {children}
         <Badge caption={badgeCaption}></Badge>
       </button>
-      {isSelected && <div className="active-tab-indicator" />}
+      {isSelected && <motion.div layoutId="tab-indicator" className="active-tab-indicator" />}
     </li>
   );
 }
 
-export default function ChallengeTabs({
-  selectedType,
-  onSelectType,
-  challenges,
-  children,
-}) {
+export default function ChallengeTabs({ selectedType, onSelectType, challenges, children }) {
   return (
     <>
       <menu id="tabs">
         <Tab
-          isSelected={selectedType === 'active'}
-          onSelect={() => onSelectType('active')}
+          isSelected={selectedType === "active"}
+          onSelect={() => onSelectType("active")}
           badgeCaption={challenges.active.length}
         >
           Active
         </Tab>
         <Tab
-          isSelected={selectedType === 'completed'}
-          onSelect={() => onSelectType('completed')}
+          isSelected={selectedType === "completed"}
+          onSelect={() => onSelectType("completed")}
           badgeCaption={challenges.completed.length}
         >
           Completed
         </Tab>
         <Tab
-          isSelected={selectedType === 'failed'}
-          onSelect={() => onSelectType('failed')}
+          isSelected={selectedType === "failed"}
+          onSelect={() => onSelectType("failed")}
           badgeCaption={challenges.failed.length}
         >
           Failed
--- react-animation/framemotion-prj/src/components/Challenges.jsx
+++ react-animation/framemotion-prj/src/components/Challenges.jsx
@@ -37,7 +37,13 @@ export default function Challenges() {
       <ChallengeTabs challenges={filteredChallenges} onSelectType={handleSelectType} selectedType={selectedType}>
         <AnimatePresence mode="wait">
           {displayedChallenges.length > 0 && (
-            <motion.ol key="list" exit={{ y: -30, opacity: 0 }} className="challenge-items">
+            <motion.ol
+              key="list"
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ y: -30, opacity: 0 }}
+              className="challenge-items"
+            >
               <AnimatePresence>
                 {displayedChallenges.map((challenge) => (
                   <ChallengeItem
