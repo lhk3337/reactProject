1d9840c86be5b3f398f553075ede898ad29c5bea
lhk3337 <lhk3337@naver.com>
2023-12-22 21:12:42 +0900

    [FEAT] ts array type

--- react_typescript/typescript_base/basics.ts
+++ react_typescript/typescript_base/basics.ts
@@ -9,3 +9,19 @@ userName = "MAX";
 let isInstructor: boolean;
 
 isInstructor = true;
+
+let hobbies: string[];
+
+hobbies = ["sports", "Cooking"];
+
+let person: any;
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
