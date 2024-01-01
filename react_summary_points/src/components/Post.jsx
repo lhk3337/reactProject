a03171a430652f80a821049702544a76497c5571
lhk3337 <lhk3337@naver.com>
2024-01-01 17:28:09 +0900

    [FEAT] react dynamic value

--- react_summary_points/src/components/Post.jsx
+++ react_summary_points/src/components/Post.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+const names = ["Max", "Manual"];
 const Post = () => {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
-      <p>Maximilian</p>
+      <p>{chosenName}</p>
       <p>React.js is aewsome!</p>
     </div>
   );
