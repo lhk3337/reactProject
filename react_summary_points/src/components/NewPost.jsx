5a241460f442e3ae13a8d1bb28498a0bb8919ea6
lhk3337 <lhk3337@naver.com>
2024-01-02 15:00:16 +0900

    [FEAT] usestate raise

--- react_summary_points/src/components/NewPost.jsx
+++ react_summary_points/src/components/NewPost.jsx
@@ -1,21 +1,15 @@
-import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-  const [value, setValue] = useState("");
-  const chnageBodyHandler = (event) => {
-    setValue(event.target.value);
-  };
+function NewPost({ onBodyChange, onAuthorChange }) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={chnageBodyHandler} />
+        <textarea id="body" required rows={3} onChange={onBodyChange} />
       </p>
-      <p>{value}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={onAuthorChange} />
       </p>
     </form>
   );
--- react_summary_points/src/components/PostsList.jsx
+++ react_summary_points/src/components/PostsList.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 const PostsList = () => {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function AuthorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
   return (
     <>
-      <NewPost />
+      <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={AuthorChangeHandler} />
       <ul className={classes.posts}>
-        <Post author="Maximilian" body="React.js is awesome!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manual" body="Check out the full course!" />
       </ul>
     </>
