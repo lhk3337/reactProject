59d607c143d2dbbc0a68a6f66bbdfff8aac45897
lhk3337 <lhk3337@naver.com>
2024-01-03 21:52:59 +0900

    [FEAT] usestate condition

--- react_summary_points/src/components/PostsList.jsx
+++ react_summary_points/src/components/PostsList.jsx
@@ -4,8 +4,13 @@ import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 const PostsList = () => {
+  const [isModalVisible, setIsModalVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setIsModalVisible(false);
+  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -16,9 +21,11 @@ const PostsList = () => {
 
   return (
     <>
-      <Modal>
-        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={AuthorChangeHandler} />
-      </Modal>
+      {isModalVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={AuthorChangeHandler} />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manual" body="Check out the full course!" />
