013e325b3292bf65636843f99142eefa58ac0401
lhk3337 <lhk3337@naver.com>
2024-01-05 21:25:05 +0900

    [FEAT] list data visible

--- react_summary_points/src/components/PostsList.jsx
+++ react_summary_points/src/components/PostsList.jsx
@@ -4,16 +4,32 @@ import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 const PostsList = ({ isPosting, onStopPosting }) => {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postsData) {
+    setPosts((existingPosts) => [postsData, ...existingPosts]);
+  }
+
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author="Manual" body="Check out the full course!" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post author={post.author} body={post.body} key={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 };
