2ca038fa122d3bde0f76fe9ab4b17d6a6a6bb814
lhk3337 <lhk3337@naver.com>
2024-01-09 22:17:10 +0900

    [FEAT] modal component route path setting

--- react_summary_points/src/components/Modal.jsx
+++ react_summary_points/src/components/Modal.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import classes from "./Modal.module.css";
+import { useNavigate } from "react-router-dom";
 
-const Modal = ({ children, onClose }) => {
+const Modal = ({ children }) => {
+  const navigate = useNavigate();
+
+  function closeHandler() {
+    navigate("/");
+  }
   return (
     <>
-      <div className={classes.backdrop} onClick={onClose} />
+      <div className={classes.backdrop} onClick={closeHandler} />
       <dialog open={true} className={classes.modal}>
         {children}
       </dialog>
